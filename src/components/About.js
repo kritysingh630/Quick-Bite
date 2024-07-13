6585b487e3b0eddf7a97ae492b87f26ffcede3bd
Krity <kritysingh20031996@gmail.com>
2024-07-13 10:49:36 +0530

    using this.state to update state variables in a class component

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <div>
             <h1>About Us</h1>
             <h2>This is a food ordering App</h2>
-            <User name={"Krity Kumari (function)"}/>
+            {/* <User name={"Krity Kumari (function)"}/> */}
             <UserClass name={"Krity Kumari (Class)"} location={"Odisha (class)"} contact={"+91-123-4567-890 (class)"}/>
         </div>
     )
--- src/components/userClass.js
+++ src/components/userClass.js
@@ -3,12 +3,24 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    
+    this.state={
+        count: 0,
+    }
   }
+
   render() {
     const {name,location,contact}=this.props;
+    const {count} = this.state
     return (
       <div className="user-card">
+        <h1>Count : {count}</h1>
+        <button onClick={()=>{
+            //NEVER UPDATE STATE VARIABLES DIRECTLY
+            this.setState({
+                count: this.state.count+1
+            })
+        }}>Increment</button>
         <h2>Name : {name}</h2>
         <h3>Location : {location}</h3>
         <h4>Contact : {contact}</h4>
